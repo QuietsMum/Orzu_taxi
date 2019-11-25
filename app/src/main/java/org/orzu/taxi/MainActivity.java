5e722c1267f91cb24cc68951db272b68fe54ca57
QuietsMum <nikita.gribanovscky@yandex.ru>
2019-11-25 01:19:15 -0500

    Initial commit

---
+++ app/src/main/java/org/orzu/taxi/MainActivity.java
@@ -0,0 +1,58 @@
+package org.orzu.taxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String MAPKIT_API_KEY = "38c4c9bc-766a-4574-8088-18a4e7583a90";
+    private final Point TARGET_LOCATION = new Point(59.945933, 30.320045);
+    private final Point TARGET_LOCATION_ALMATY = new Point(43.25654, 76.92848);
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /**
+         * Задайте API-ключ перед инициализацией MapKitFactory.
+         * Рекомендуется устанавливать ключ в методе Application.onCreate,
+         * но в данном примере он устанавливается в activity.
+         */
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        /**
+         * Инициализация библиотеки для загрузки необходимых нативных библиотек.
+         * Рекомендуется инициализировать библиотеку MapKit в методе Activity.onCreate
+         * Инициализация в методе Application.onCreate может привести к лишним вызовам и увеличенному использованию батареи.
+         */
+        MapKitFactory.initialize(this);
+        // Создание MapView.
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        mapView = (MapView)findViewById(R.id.mapview);
+        // Перемещение камеры в центр Санкт-Петербурга.
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION_ALMATY, 14.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 5),
+                null);
+    }
+    @Override
+    protected void onStop() {
+        // Вызов onStop нужно передавать инстансам MapView и MapKit.
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+    @Override
+    protected void onStart() {
+        // Вызов onStart нужно передавать инстансам MapView и MapKit.
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+}
